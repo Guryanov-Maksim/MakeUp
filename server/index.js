e72a8592ee7aeb8c48bad9a2201249dabfeb81a9
Guryanov-Maksim <guryanov-mv@yandex.ru>
2023-05-27 13:24:00 +0300

    refactor: backend https

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ import { fileURLToPath } from 'url';
 // const html = fs.readFileSync(`${process.cwd()}/dist/index.html`, 'utf-8');
 const __dirname = fileURLToPath(path.dirname(import.meta.url));
 // console.log(path.resolve(__dirname, '..', 'dist'))
-export const options = {
-  http2: true,
-  https: {
-    key: fs.readFileSync(path.join(__dirname, '..', 'secrets', 'localhost.key')),
-    cert: fs.readFileSync(path.join(__dirname, '..', 'secrets', 'localhost.crt'))
-  }
-}
+// export const options = {
+//   http2: true,
+//   https: {
+//     key: fs.readFileSync(path.join(__dirname, '..', 'secrets', 'localhost.key')),
+//     cert: fs.readFileSync(path.join(__dirname, '..', 'secrets', 'localhost.crt'))
+//   }
+// }
 
 const app = async (fastify, opts) => {
   fastify.register(fastifyStatic, {
